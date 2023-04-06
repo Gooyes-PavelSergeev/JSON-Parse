a6d5baa8d3d1417e0fe0e0478b474adb8cd5787c
Pavel <pashaveegres@mail.ru>
2023-04-06 23:06:41 +0300

    Init

---
+++ Assets/Core/GeoJSONObject.cs
@@ -0,0 +1,41 @@
+﻿namespace GeoJSON {
+
+	public class GeoJSONObject {
+
+		public string type;
+
+		public GeoJSONObject() {
+		}
+
+		public GeoJSONObject(JSONObject jsonObject) {
+			if(jsonObject != null)
+				type = jsonObject ["type"].stringValue;
+		}
+
+		static public FeatureCollection Deserialize(string encodedString) {
+			FeatureCollection collection;
+
+			JSONObject jsonObject = new JSONObject (encodedString);
+			if (jsonObject ["type"].stringValue == "FeatureCollection") {
+				collection = new FeatureCollection (jsonObject);
+			} else {
+				collection = new FeatureCollection ();
+				collection.features.Add (new FeatureObject (jsonObject));
+			}
+
+			return collection;
+		}
+
+		virtual public JSONObject Serialize () {
+
+			JSONObject rootObject = new JSONObject (JSONObject.Type.Object);
+			rootObject.AddField ("type", type);
+
+			SerializeContent (rootObject);
+
+			return rootObject;
+		}
+
+		protected virtual void SerializeContent(JSONObject rootObject) {}
+	}
+}
---
+++ Assets/Core/PositionObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GeoJSON {
+
+	[Serializable]
+	public class PositionObject {
+		public float latitude;
+		public float longitude;
+
+		public PositionObject() {
+		}
+
+		public PositionObject(float pointLongitude, float pointLatitude) {
+			longitude = pointLongitude;
+			latitude = pointLatitude;
+		}
+
+		public PositionObject(JSONObject jsonObject) {
+			longitude = jsonObject.list [0].floatValue;
+			latitude = jsonObject.list [1].floatValue;
+		}
+
+		public JSONObject Serialize() {
+
+            JSONObject jsonObject = new JSONObject(JSONObject.Type.Array)
+            {
+                longitude,
+                latitude
+            };
+
+            return jsonObject;
+		}
+
+		override public string ToString() {
+			return longitude + "," + latitude;
+		}
+
+		public float[] ToArray() {
+
+			float[] array = new float[2];
+
+			array [0] = longitude;
+			array [1] = latitude;
+
+			return array;
+		}
+	}
+}
